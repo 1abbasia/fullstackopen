6ecbb8cc689e4f3e9f9a25d6513801e5ffabd8d6
Ahsan Abbasi <1abbasia@gmail.com>
2022-03-19 03:01:10 -0400

    unicafe step 2 evrything works except all stat

--- part1/unicafe/unicafe/src/App.js
+++ part1/unicafe/unicafe/src/App.js
@@ -28,7 +28,7 @@ const StatDisplay = (props) => {
       <div>neutral:{props.neutralCount}</div>
       <div>bad:{props.badCount}</div>
       <div>all:{props.allCount}</div>
-      <div>average score:{props.average}</div>
+      <div>average score:{props.avg}</div>
       <div>Positive percentage:{props.posPercent}</div>
     </div>
   )
@@ -40,12 +40,22 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+
   //create on click event for  button. It increments out good variable count by 1 using hooks on line 36-38
   const handleGoodCounter = () => {setGood(good +1)};
 
   const handleNeutralCounter = () => {setNeutral(neutral +1)};
 
   const handleBadCounter = () => {setBad(bad +1)};
+
+  //logic for average score varibale based on the good, neutral and bad values as good=1, neutral =0, bad =-1
+  const avgValues = ((good*1) + (neutral*0) + (bad*0))/3;
+
+  //logic for displaying all count 
+  const allCount = good + neutral + bad;
+  
+  const pos = (good/allCount)*100;
+  console.log(avgValues);
   
 
   
@@ -54,7 +64,7 @@ const App = () => {
     <div>
       <Header/>
       <ButtonDisplay handleGoodCounter = {handleGoodCounter} handleNeutralCounter = {handleNeutralCounter} handleBadCounter = {handleBadCounter}/>
-      <StatDisplay goodCount = {good} neutralCount = {neutral} badCount = {bad}/>
+      <StatDisplay goodCount = {good} neutralCount = {neutral} badCount = {bad} avg ={avgValues} all ={allCount} posPercent ={pos}/>
     </div>
   )
 }
